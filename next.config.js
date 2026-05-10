51b261e97fbb16d99858441fd98b5b87e21dc5a7
saori095 <sghdkakita@gmail.com>
2026-05-10 20:27:50 +0900

    yml修正#5

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import("next").NextConfig} */
-const path = require('path');
 const API_PROXY_TARGET = process.env.API_PROXY_TARGET || 'http://127.0.0.1:8000';
 
 module.exports = {
   output: 'standalone',
-  turbopack: {
-    root: path.join(__dirname),
-  },
 
   async rewrites() {
     return [
