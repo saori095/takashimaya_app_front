f6f23dc0c0173f229508e07493721e8b81b156dc
saori095 <sghdkakita@gmail.com>
2026-05-10 19:36:42 +0900

    next.config修正

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
-/** @type {import("next").NextConfig} */
-const API_PROXY_TARGET = process.env.API_PROXY_TARGET || 'http://127.0.0.1:8000';
+require('dotenv').config()
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'standalone'
+}
 
-module.exports = {
-  output: 'standalone',
-
-  async rewrites() {
-    return [
-      { source: '/api/:path*', destination: `${API_PROXY_TARGET}/:path*` },
-      { source: '/uploads/:path*', destination: `${API_PROXY_TARGET}/uploads/:path*` },
-    ];
-  },
-};
+module.exports = nextConfig
